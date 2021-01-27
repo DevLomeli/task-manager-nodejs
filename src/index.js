afe0d4b00bc7827b85e22ea8de687253479cee01
Fernando Lomeli <fercho0.fpl@gmail.com>
2021-01-27 17:25:26 -0600

    Init commit

---
+++ src/index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+require("./db/mongoose");
+
+const userRouter = require("./routes/user");
+const taskRouter = require("./routes/task");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const multer = require("multer");
+const upload = multer({
+  dest: "images",
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+      return cb(new Error("Please upload a word document"));
+    }
+    cb(undefined, true);
+
+    // cb(new Error("File must be a PDF"));
+    // cb(undefined, true);
+    // cb(undefined, false);
+  },
+});
+const errorMiddleware = (req, res, next) => {
+  throw new Error("From my middleware");
+};
+app.post("/upload", errorMiddleware, (req, res) => {
+  res.send();
+});
+
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
+
+app.listen(port, () => {
+  console.log("Server is on port " + port);
+});
+
+// const Task = require("./models/task");
+// const User = require("./models/user");
+
+// const main = async () => {
+// const task = await Task.findById("6010dbda61404c201cc73f22");
+// await task.populate("owner").execPopulate();
+// console.log(task.owner);
+
+//   const user = await User.findById("6010db5261404c201cc73f20");
+//   await user.populate("tasks").execPopulate();
+//   console.log(user.tasks);
+// };
+
+// main();
